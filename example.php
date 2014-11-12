0d7bd4dcea8974409b475b2f0a421d152cba5206
Sercan Çakır <srcnckr@gmail.com>
2014-11-12 22:27:57 +0200

    Update example.php

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
 require 'less_to_scss.php';
 
 $file = file_get_contents(dirname(__DIR__) . '/assets/less/custom.less');
-$output = Convert::less_to_css($file);
+$output = Convert::less_to_scss($file);
 
 echo $output;
 
